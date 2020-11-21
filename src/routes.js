01e70ba5a16f9cf7f7de6ac02c30a1e47933640e
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-11-21 17:41:00 -0300

    Remove rotas não utilizadas

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,12 @@ const express = require('express')
 
 const routes = express.Router()
 
-const CourseController = require('./controllers/CourseController');
-const AuthorController = require('./controllers/AuthorController');
 const LocalizacaoController = require('./controllers/LocalizacaoController');
 const EventoController = require('./controllers/EventoController');
 const PessoaController = require('./controllers/PessoaController');
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to our API' }))
 
-routes.use('/courses', CourseController);
-routes.use('/authors', AuthorController);
 routes.use('/localizacoes', LocalizacaoController)
 routes.use('/eventos', EventoController)
 routes.use('/pessoas', PessoaController)
