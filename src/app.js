caf0b71bc4646d53f5912f46ea77e8d394f10f61
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-11-26 10:44:50 -0300

    Adiciona cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./routes')
@@ -9,6 +10,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
